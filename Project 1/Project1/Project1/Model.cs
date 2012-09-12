4a84d0039c9a53b1c3874a260bc3e8de42e472cf
SJ Ong <shingjay.ong@gmail.com>
2012-09-11 21:51:19 -0400

    Everything

---
+++ Project 1/Project1/Project1/Model.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.Modeling;
+using TFTPClientNameSpace;
+
+namespace Project1
+{
+    // model 
+
+    /// <summary>
+    /// An example model program.
+    /// </summary>
+    static class ModelProgram
+    {
+        static TFTPClient.FSM_Modes mode;
+
+        [Rule(Action = "InitializeTestAdapter()")]
+        static void Initialize()
+        {
+            mode = TFTPClient.FSM_Modes.INIT;
+        }
+
+        [Rule(Action = "AdapterSendReadRequest()")]
+        static void SendReadRequest()
+        {
+            Condition.IsTrue(mode == TFTPClient.FSM_Modes.INIT);
+            mode = TFTPClient.FSM_Modes.RRQ_SENT;
+        }
+
+        [Rule(Action = "AdapterReceiveDataBlock()")]
+        static void ReceiveDataBlock()
+        {
+            Condition.IsTrue(mode == TFTPClient.FSM_Modes.RRQ_SENT || mode == TFTPClient.FSM_Modes.ACK_SENT);
+            mode = TFTPClient.FSM_Modes.DATA_RECEIVED;
+        }
+
+        [Rule(Action = "AdapterSendACK()")]
+        static void SendACK()
+        {
+            Condition.IsTrue(mode == TFTPClient.FSM_Modes.DATA_SENT);
+            mode = TFTPClient.FSM_Modes.ACK_SENT;
+        }
+
+        [Rule(Action = "AdapterSendWriteRequest()")]
+        static void SendWriteRequest()
+        {
+            Condition.IsTrue(mode == TFTPClient.FSM_Modes.INIT);
+            mode = TFTPClient.FSM_Modes.WRQ_SENT;
+        }
+
+        [Rule(Action = "AdapterSendDataBlock()")]
+        static void SendDataBlock()
+        {
+            Condition.IsTrue(mode == TFTPClient.FSM_Modes.ACK_RECEIVED);
+            mode = TFTPClient.FSM_Modes.DATA_SENT;
+        }
+
+        [Rule(Action = "AdapterReceiveACK()")]
+        static void ReceiveACK()
+        {
+            Condition.IsTrue(mode == TFTPClient.FSM_Modes.WRQ_SENT || mode == TFTPClient.FSM_Modes.DATA_SENT);
+            mode = TFTPClient.FSM_Modes.ACK_RECEIVED;
+        }
+
+        [Rule(Action = "AdapterCanPutExit(len)")]
+        static bool CanPutExit(int len)
+        {
+            Condition.IsTrue(mode == TFTPClient.FSM_Modes.DATA_SENT);
+            if (len > 516)
+            {
+                mode = TFTPClient.FSM_Modes.EXIT;
+                return true;
+            }
+            return false;
+        }
+
+        [Rule(Action = "AdapterCanGetExit(len)")]
+        static bool CanGetExit(int len)
+        {
+            Condition.IsTrue(mode == TFTPClient.FSM_Modes.DATA_RECEIVED);
+            if (len > 516)
+            {
+                mode = TFTPClient.FSM_Modes.EXIT;
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Project 1/Project1/TestAdapter/AdapterClass.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using TFTPClientNameSpace;
+
+namespace TestAdapter
+{
+    public class AdapterClass
+    {
+        private static TFTPClient client;
+
+        static void InitializeTestAdapter()
+        {
+            client = new TFTPClient("127.0.0.1");
+            client.initialize();    
+        }
+
+        static void AdapterSendReadRequest()
+        {
+         //   client.sendReadRequest();
+        }
+
+        static void AdapterReceiveDataBlock()
+        {
+        }
+
+        static void AdapterSendACK()
+        {
+        }
+
+        static void AdapterCanGetExit(int len)
+        {
+        }
+
+        static void AdapterSendWriteRequest()
+        {
+
+        }
+
+        static void AdapterSendDataBlock()
+        {
+        }
+
+        static void AdapterReceiveACK()
+        {
+        }
+
+        static void AdapterCanPutExit(int len)
+        {
+        }
+    }
+}
