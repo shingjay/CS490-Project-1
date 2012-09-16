5620936998bc243ead5b85def46240f16db13703
Su Lin Ooi <sulinooi16@gmail.com>
2012-09-16 16:48:06 -0400

    adapter class

--- Project 1/Project1/TestAdapter/AdapterClass.cs
+++ Project 1/Project1/TestAdapter/AdapterClass.cs
@@ -9,17 +9,63 @@ namespace Project1.TestAdapter
 {
     public class AdapterClass
     {
-        private static TFTPClient client;
+        public static TFTPClient client = InitializeTestAdapter();
+        public static string remoteFile = @"remote.txt";
+        public static string localFile = createLocalFilePath();
 
-        static void InitializeTestAdapter()
+        static string createLocalFilePath()
         {
-            client = new TFTPClient("127.0.0.1");
-            client.initialize();    
+            // Specify a "currently active folder" 
+            string activeDir = @"C:\Users\Sulin\Documents\Proj1TestDir";
+
+            //Create a new subfolder under the current active folder 
+            string newPath = System.IO.Path.Combine(activeDir, "SubDir");
+
+            // Create the subfolder
+            System.IO.Directory.CreateDirectory(newPath);
+
+            // Create a new file name. This example generates 
+            // a random string. 
+            string newFileName = System.IO.Path.GetRandomFileName();
+
+            // Combine the new file name with the path
+            newPath = System.IO.Path.Combine(newPath, newFileName);
+
+            // Create the file and write to it. 
+            // Create will overwrite the file 
+            // if it already exists.
+            if (!System.IO.File.Exists(newPath))
+            {
+                using (System.IO.FileStream fs = System.IO.File.Create(newPath))
+                {
+                    for (byte i = 0; i < 100; i++)
+                    {
+                        fs.WriteByte(i);
+                    }
+                }
+            }
+
+            return newPath;
+        }
+
+        public static void Checker(int state)
+        {
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual
+            (state, (int)client.tftpClientMode, "AdapterClass mismatch: Model vs Implimentation.");
+        }
+
+        static TFTPClient InitializeTestAdapter()
+        {
+            TFTPClient c = new TFTPClient("127.0.0.1", 69);
+            c.initialize();
+
+            localFile = createLocalFilePath();
+            return c;
         }
 
         static void AdapterSendReadRequest()
         {
-         //   client.sendReadRequest();
+            client.sendReadRequest(remoteFile, localFile, TFTPClient.Modes.NetAscii);
         }
 
         static void AdapterReceiveDataBlock()
